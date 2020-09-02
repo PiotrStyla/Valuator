b7937c0fb000ae99ff7523ec4a1d2953fea1367e
Piotr Styła <31997939+PiotrStyla@users.noreply.github.com>
2020-09-02 21:28:36 +0200

    Navigation Trial

---
+++ app/src/main/java/pl/aplikacje/valuator/ValuePageFragment.kt
@@ -0,0 +1,42 @@
+package pl.aplikacje.valuator
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+
+class ValuePageFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_value_page, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.button_settings).setOnClickListener(this)
+        view.findViewById<Button>(R.id.camera_new_capture_button).setOnClickListener(this)
+        view.findViewById<Button>(R.id.button_history).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.camera_new_capture_button -> navController!!.navigate(R.id.action_valuePageFragment_to_mainFragment)
+            R.id.button_settings -> navController!!.navigate(R.id.action_valuePageFragment_to_settingsFragment)
+            R.id.button_history-> navController!!.navigate(R.id.action_valuePageFragment_to_historyFragment)
+
+        }
+    }
+}
