9369db38e1665673f5883cbb800e15ecc579efbd
Piotr Styła <31997939+PiotrStyla@users.noreply.github.com>
2020-09-07 16:15:17 +0200

    Room repository added

---
+++ app/src/main/java/pl/aplikacje/valuator/repository/AppReository.kt
@@ -0,0 +1,18 @@
+package pl.aplikacje.valuator.repository
+
+import androidx.lifecycle.LiveData
+import pl.aplikacje.valuator.database.CarPhotoInDatabase
+import pl.aplikacje.valuator.database.ItemDao
+
+// Declares the DAO as a private property in the constructor. Pass in the DAO
+// instead of the whole database, because you only need access to the DAO
+class AppReository(private val itemDao: ItemDao) {
+
+    // Room executes all queries on a separate thread.
+    // Observed LiveData will notify the observer when the data has changed.
+    val allItems: LiveData<List<CarPhotoInDatabase>> = itemDao.getAll()
+
+    suspend fun insert(item: CarPhotoInDatabase) {
+        itemDao.insert(item)
+    }
+}
